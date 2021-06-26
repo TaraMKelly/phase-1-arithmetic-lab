c5206b0c71aadc27359a6f2bfd545fd7b17e390f
TaraMKelly <tara.kelly16@gmail.com>
2021-06-26 11:40:51 -0400

    commit tree cb4fcce6fe1fd8fc759febb0a93ec05d78e2cc3b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10);
+
+let num3 = 16;
+let num4 = 6;
+const mod = 16 % 6;
+
+const max = Math.max(10,15,20);
